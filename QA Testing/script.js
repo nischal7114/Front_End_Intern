351e29aecf80c60c5432969238fc866dc05e21f9
Nischal Shrestha <nischal.shrestha@groupiig.com>
2025-04-17 15:20:11 +0545

    k6 Testing

---
+++ QA Testing/script.js
@@ -0,0 +1,8 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export default function () {
+    const res = http.get('https://groupiig.techarttrekkies.com.np/');
+    console.log('Status code: ${res.status}');
+    sleep(1);
+}
