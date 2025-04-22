35f15d416358fac69eb97a99a53f3fa301d755a6
Nischal Shrestha <nischal.shrestha@groupiig.com>
2025-04-22 17:13:08 +0545

    String Methods in JS

---
+++ JavaScript/Concepts/string_methods.js
@@ -0,0 +1,19 @@
+let name = "nischal";
+
+console.log(name.length);
+
+console.log(name.toUpperCase());
+
+console.log(name.toLowerCase());
+
+console.log(name.slice(2,4));
+
+console.log(name.slice(2));
+
+console.log(name.replace("schal", "kku"));
+
+let lname = "shrestha";
+console.log(name.concat(" ", lname, "is an intern at Techart Trekkies"));
+
+let wname = "  What's up?  ";
+console.log(wname.trim());
