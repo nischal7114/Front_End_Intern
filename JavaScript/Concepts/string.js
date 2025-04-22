07f0c934d21c215e7d497da58ec650852c0c2faf
Nischal Shrestha <nischal.shrestha@groupiig.com>
2025-04-22 16:12:56 +0545

    String in JS

---
+++ JavaScript/Concepts/string.js
@@ -0,0 +1,14 @@
+let name = `\"nischal\"`;
+console.log(name);
+console.log(name.length);
+
+for(let i = 0; i < name.length; i++)
+{
+    console.log(name[i]);    
+}
+
+let fname = 'Nischal';
+let lname = 'Shrestha';
+let sentence = `My name is "${fname} ${lname}"`;
+console.log(sentence);
+
