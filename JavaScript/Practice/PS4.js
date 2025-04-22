2eb15728188ca5106d3e0a784505a12e4154c567
Nischal Shrestha <nischal.shrestha@groupiig.com>
2025-04-22 18:00:44 +0545

    Practice Sets on Script adn String Methods

---
+++ JavaScript/Practice/PS4.js
@@ -0,0 +1,17 @@
+//Q1. What will the following print in JavaScript? 
+//Console.log("har\"".length)
+console.log("har\"".length);
+
+//Q2. Explore the includes, starts with & ends with functions of a string
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+const word = 'fox';
+const isincluded = sentence.includes(word);
+console.log(isincluded);
+console.log(`The word "${word} ${sentence.includes(word)? 'is' : 'is not'} in the sentence`);
+
+//Q3. Write a program to convert a given string to lowercase
+
+//Q4. Extract the amount out of this string
+//" Please give Rs 1000"
+
+//Q5. Try to change 4th character of a given string. Were you able to do it?
