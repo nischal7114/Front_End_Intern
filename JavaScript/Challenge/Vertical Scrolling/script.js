c8711803ccc64e90d4c4d06bf68a7d9d555990ed
Nischal Shrestha <nischal.shrestha@groupiig.com>
2025-05-25 18:00:00 +0545

    working on vertical scrolling

---
+++ JavaScript/Challenge/Vertical Scrolling/script.js
@@ -0,0 +1,6 @@
+let currentIndex = 0;
+let isDragging = false;
+let startY = 0;
+const container = document.querySelector(".container");
+const cards = document.querySelectorAll(".card");
+const cardList = document.querySelector(".card")
